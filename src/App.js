69b56043651a10faf81eeea0d40a7da4d4eae1c5
AkshatSoni26 <135410361+AkshatSoni26@users.noreply.github.com>
2024-04-09 15:39:43 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { getDatabase, ref, set } from "firebase/database";
+import firebaseapp from './firebase.js';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+
+const auth = getAuth(firebaseapp);
+// const db = getDatabase(firebaseapp);
+const provider = new GoogleAuthProvider();
 
 function App() {
+
+  // const putData = () => {
+  //   set(ref(db, 'users/akshat'), {
+  //     username: 'akshat',
+  //     email: "email@gmail.com",
+  //     age: 21
+  //   });
+  // }
+
+  const siguupUser = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        console.log('result ====>', result)
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        console.log('errors ===>', error)
+        // ...
+      });
+    // createUserWithEmailAndPassword(auth, 'akshatsoni2604@gmail.com', 'Akshat@123').then((userCredential) => {
+    //   // Signed up 
+    //   const user = userCredential.user;
+    //   console.log('usercredentials ===>', userCredential)
+    //   // ...
+    // })
+    //   .catch((error) => {
+    //     const errorCode = error.code;
+    //     const errorMessage = error.message;
+    //   console.log('error ===>', error)
+    //     // ..
+    //   });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={siguupUser}>Sign Up</button>
+      {/* <button onClick={putData}>Put Data</button> */}
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,9 +15,16 @@ const firebaseConfig = {
   storageBucket: "chat-app-1bdba.appspot.com",
   messagingSenderId: "250769165998",
   appId: "1:250769165998:web:aeae314e75cedafc2fa73f",
-  measurementId: "G-327H84D9Z4"
+  measurementId: "G-327H84D9Z4",
+  databaseURL:'https://chat-app-1bdba-default-rtdb.firebaseio.com'
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const firebaseapp = initializeApp(firebaseConfig);
+export const firebaseAnalytics = getAnalytics(firebaseapp);
+
+
+// Initialize Firebase Authentication and get a reference to the service
+export const firbaseAuth = getAuth(firebaseapp);
+
+export default firebaseapp
