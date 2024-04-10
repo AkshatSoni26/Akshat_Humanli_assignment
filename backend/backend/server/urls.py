af9a0fc968c647c9b026fa0f2cdb2a4ee2a7e8c8
akshatsoni26 <akshatsoni2604@gmail.com>
2024-04-10 23:41:18 +0530

    .

--- backend/backend/server/urls.py
+++ backend/backend/server/urls.py
@@ -4,16 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    # path('', views.home, name='home' ),
     path('login/', views.login, name='login' ),
-    # path('register/', views.register, name='register' ),
-
-    # path('get-todos/', views.get_todos, name='get_todos'),
 
     path('create-chat/', views.create_chat, name='create_chat'),
-    # path('update-chat/', views.update_chat, name='update_chat'),
-
-    # path('delete-chat/<int:todo_id>/', views.delete_chat, name='delete_chat'),
-    # path('update-todo/<int:todo_id>/', views.update_todo, name='update_todo'),
-
 ]
